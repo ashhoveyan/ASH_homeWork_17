d6534ecc399045a02417fd597ae09bbca904d402
Ashot Hoveyan <ash.hoveyan.02@mai.ru>
2024-08-30 02:00:15 +0400

    added models

---
+++ models/Books.js
@@ -0,0 +1,43 @@
+import {Model, DataTypes} from "sequelize";
+import sequelize from "../clients/sequelize.mysql.js";
+
+
+class Books extends Model {}
+
+Books.init(
+    {
+        id: {
+            type: DataTypes.BIGINT.UNSIGNED,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        author: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        userId: {
+            type: DataTypes.BIGINT.UNSIGNED,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: true,
+        modelName: 'books',
+        tableName: 'books',
+        indexes: [
+            {
+                unique: true,
+                fields: ['title'],
+            },
+        ]
+    }
+);
+
+
+export default Books;
--- models/Users.js
+++ models/Users.js
@@ -1,1 +1,49 @@
-i
+import {Model,DataTypes} from 'sequelize';
+
+import sequelize from "../clients/sequelize.mysql.js";
+
+import Books from "./Books.js";
+
+class Users extends Model {}
+
+
+Users.init(
+    {
+        id: {
+            type: DataTypes.BIGINT.UNSIGNED,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
+    },
+    {
+        sequelize,
+        timestamps: true,
+        modelName: 'users',
+        tableName: 'users',
+        indexes: [
+            {
+                unique: true,
+                fields: ['username'],
+            },
+        ]
+    }
+)
+Users.hasMany(Books,{
+    onDelete: "CASCADE",
+    onUpdate: "CASCADE",
+    foreignKey: "userId",
+})
+Books.belongsTo(Users)
+
+
+
+export default Users;
