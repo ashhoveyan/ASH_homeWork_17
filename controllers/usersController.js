57140a68e20660bc19b4faceb73eb126b0b3a36b
Ashot Hoveyan <ash.hoveyan.02@mai.ru>
2024-08-30 03:02:56 +0400

    added users controllers

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,5 +1,81 @@
-import models from '../models/Users.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+import Users from '../models/Users.js';
 
 export default {
+    async registration(req, res) {
+        try{
+            const {username, password} = req.body;
+            const hashedPassword = await bcrypt.hash(password + process.env.SECRET_FOR_PASSWORD, 10);
+            const [user, created] = await Users.findOrCreate({
+                where: { username: username },
+                defaults: {
+                    username: username,
+                    password: hashedPassword,
+                }
+            });
+            if (!created) {
+                return res.status(409).json({
+                    message: 'User already exists',
+                });
+            } else {
+                user.password = undefined
+                return res.status(201).json({
+                    message: 'User created successfully',
+                    user: user,
+                });
+            }
+        }catch (error) {
+            console.error('Registration Error:', error);
+            return res.status(500).json({
+                message: 'registration failed',
+                error: error.message,
+            });
+        }
+    },
+    async login(req, res) {
+        try {
+            const { username, password } = req.body;
+            const user = await Users.findOne({
+                where: { username: username }
+            });
+            if (!user) {
+                return res.status(400).json({
+                    message: 'Invalid username or password'
+                });
+            }
+            const payload = {
+                username: user.username,
+                id: user.id
+            };
+
+            const token = jwt.sign(
+                payload,
+                process.env.SECRET_FOR_JWT, {
+                expiresIn: '1h'
+            });
+
+
+            console.log(token)
+            req.headers.authorization = token;
+
+            //user.password = undefined
+
+            return res.status(200).json({
+                message: 'User logged in successfully',
+                user: user,
+                token: token
+            });
+
+
+        } catch (error) {
+            console.error('Login Error:', error);
+            return res.status(500).json({
+                message: 'Login failed',
+                error: error.message
+            });
+        }
+    }
 
 }
